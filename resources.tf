8a29a0569f34ce73a28a1e50383aa88d2a91a5a3
Igor Moskalev <moskalev.igor.v@gmail.com>
2024-09-30 08:58:29 +0500

    fix: format fix

--- resources.tf
+++ resources.tf
@@ -7,21 +7,21 @@ resource "aws_s3_bucket" "terraform-states" {
 resource "aws_iam_role" "GithubActionsRole" {
   name = var.terraform_github_actions_role_name
   assume_role_policy = jsonencode({
-      "Version" : "2012-10-17",
-      "Statement" : [
-        {
-          "Effect" : "Allow",
-          "Principal" : {
-            "Federated" : "arn:aws:iam::${var.aws_account_id}:oidc-provider/token.actions.githubusercontent.com"
-          },
-          "Action" : "sts:AssumeRoleWithWebIdentity",
-          "Condition" : {
-            "StringEquals" : {
-              "token.actions.githubusercontent.com:sub" : "repo:${var.github_repository_name}:*"
-            }
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Federated" : "arn:aws:iam::${var.aws_account_id}:oidc-provider/token.actions.githubusercontent.com"
+        },
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Condition" : {
+          "StringEquals" : {
+            "token.actions.githubusercontent.com:sub" : "repo:${var.github_repository_name}:*"
           }
         }
-      ]
+      }
+    ]
     }
   )
 }
