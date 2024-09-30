22e5564a914ed4e84c5b3748896ac33ea2fbef0b
Igor Moskalev <moskalev.igor.v@gmail.com>
2024-09-30 09:12:49 +0500

    fix: format fix

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "rss-terraform-states"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
+    bucket  = "rss-terraform-states"
+    key     = "terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
   }
 }
